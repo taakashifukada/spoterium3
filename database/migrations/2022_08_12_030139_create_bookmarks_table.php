ec7eb3052f78cbd5a60007bab6a1645c2ff7f371
taakashifukada <takashifukada98@gmail.com>
2022-08-22 21:13:13 +0900

    first commit

---
+++ database/migrations/2022_08_12_030139_create_bookmarks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('url',200);
+            $table->string('title',50);
+            $table->string('img_path',200)->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->string('comment',200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+};
